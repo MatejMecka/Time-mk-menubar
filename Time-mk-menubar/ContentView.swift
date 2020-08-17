3c12a554779af7ba14370e5d8292a9134d8e5111
matejmecka <matej.plavevski+github@gmail.com>
2020-08-17 23:05:13 +0200

    Initial Commit

---
+++ Time-mk-menubar/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Time-mk-menubar
+//
+//  Created by Matej Plavevski on 8/17/20.
+//  Copyright © 2020 Matej Plavevski. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
